3bfcc6428ec4f974571dfdac78963725addb94b1
jijek <maseug@icloud.com>
2023-09-23 03:24:13 +0600

    Time: 58 ms (62.46%), Space: 44 MB (62.18%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.js
+++ 0252-meeting-rooms/0252-meeting-rooms.js
@@ -6,14 +6,10 @@ var canAttendMeetings = function(intervals) {
     const n = intervals.length;
     intervals.sort((a,b) => a[0] - b[0]);
     
-    for (let i=0; i<n; i++) {
-        const el = intervals[i][1];
-        
-        for (let j=i+1; j<n; j++) {{
-            if (el > intervals[j][0]) {
-                return false
-            }
-        }}
+    for (let i=0; i<n-1; i++) {
+        if (intervals[i][1] > intervals[i+1][0]) {
+            return false
+        }
     }
     
     return true
