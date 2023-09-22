38cabe667d98ee2436ff532a240d8fe8542fdb53
jijek <maseug@icloud.com>
2023-09-22 20:05:49 +0600

    Time: 35 ms (99.71%), Space: 42.3 MB (36.80%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+var maximumWealth = function(accounts) {
+    const n = accounts.length;
+    let ans = [];
+    
+    for (let i=0; i<n; i++) {
+        const el = accounts[i].reduce((a,b) => a+b, 0);
+        
+        ans[i] = el;
+    }
+    
+    return Math.max.apply(null, ans)
+};
