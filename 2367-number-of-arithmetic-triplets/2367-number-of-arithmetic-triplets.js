0c4708184b7d45197099b9557c3f9e6465f0bf94
jijek <maseug@icloud.com>
2023-09-24 04:23:49 +0600

    Time: 73 ms (19.05%), Space: 42.2 MB (46.86%) - LeetHub

---
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} diff
+ * @return {number}
+ */
+var arithmeticTriplets = function(nums, diff) {
+    let count = 0;
+    const n = nums.length;
+    
+    for (let i=0; i<n; i++) {
+        for (let j=i+1; j<n; j++) {
+            for (let k=j+1; k<n; k++) {
+                if (nums[j] - nums[i] === diff && nums[k] - nums[j] === diff) {
+                    count++
+                }
+            }
+        }
+    }
+    
+    return count
+};
