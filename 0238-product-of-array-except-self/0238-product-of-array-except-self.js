ba08b5bfb46432b4b685610bc8462fbc402029de
jijek <maseug@icloud.com>
2023-09-27 01:51:51 +0600

    Time: 82 ms (79.36%), Space: 53.5 MB (77.94%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -4,11 +4,11 @@
  */
 var productExceptSelf = function(nums) {
     const n = nums.length;
-    let left = Array(n).fill(1);
+    let left = [1];
     let right = Array(n).fill(1);
     
     for (let i=1; i<n; i++) {
-        left[i] = left[i-1] * nums[i-1]
+        left[i] = left[i-1] * nums[i-1];
     }
     
     for (let i=n-2; i>=0; i--) {
