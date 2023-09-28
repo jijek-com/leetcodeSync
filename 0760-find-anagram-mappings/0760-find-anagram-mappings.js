0fe61d80b4a341c5436adcb09b6eccb399c6717a
jijek <maseug@icloud.com>
2023-09-29 02:14:57 +0600

    Time: 56 ms (27.42%), Space: 41.8 MB (74.19%) - LeetHub

--- 0760-find-anagram-mappings/0760-find-anagram-mappings.js
+++ 0760-find-anagram-mappings/0760-find-anagram-mappings.js
@@ -4,16 +4,16 @@
  * @return {number[]}
  */
 var anagramMappings = function(nums1, nums2) {
-    const n = nums2.length;
     const hash = {};
+    const n = nums1.length;
     
     for (let i=0; i<n; i++) {
         hash[nums2[i]] = i;
     }
     
     for (let i=0; i<n; i++) {
-        nums2[i] = hash[nums1[i]]
+        nums1[i] = hash[nums1[i]]
     }
     
-    return nums2
+    return nums1
 };
