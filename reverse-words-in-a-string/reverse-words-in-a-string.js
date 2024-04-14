26fa3eaf045f0d8b901a9e272f8a6f6bbc2c9523
jijek <maseug@icloud.com>
2024-04-15 04:38:13 +0500

    Time: 50 ms, Memory: 51.1 MB - LeetHub

--- reverse-words-in-a-string/reverse-words-in-a-string.js
+++ reverse-words-in-a-string/reverse-words-in-a-string.js
@@ -3,15 +3,8 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    let ans = [];
-    let str = s.split(' ');
-    const n = str.length;
-    
-    for (let i=0; i < n; i++) {
-        if (str[i] !== '') {
-            ans.unshift(str[i])
-        }
-    }
-    
+    const regexp = /\s+/g;
+    let ans = s.trim().replace(regexp, ' ').split(' ').reverse();
+
     return ans.join(' ')
 };
